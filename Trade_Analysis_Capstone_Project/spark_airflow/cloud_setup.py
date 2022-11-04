e5ac8cf433b7c21a92de19db18f6e1adca4661c3
Seethalakshmi Rengaraman <seethalakshmi.kannan@gmail.com>
2022-11-03 22:11:20 -0400

    Airflow pipeline updated

---
+++ Trade_Analysis_Capstone_Project/spark_airflow/cloud_setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+This module is to setup cloud storage bucket and use it to ingest and process stock
+market data
+"""
+from google.cloud import storage
+from google.oauth2 import service_account
+import logging
+
+class CloudSetup:
+    def __init__(self,bucket_name):
+        #self.credentials = service_account.Credentials.from_service_account_file(
+    #'token.json')
+        self.storage_client = storage.Client()
+        self.bucket_name = bucket_name
+        log_format = '%(asctime)s %(module)s %(name)s.%(funcName)s +%(lineno)s: %(levelname)-2s [%(process)d] %(message)s'
+        logging.basicConfig(level=logging.INFO,
+                        filename='Trade_Analysis.logs',
+                        format=log_format,
+                        datefmt='%Y-%m-%d %H:%M:%S'
+                        )
+        self.create_gcp_bucket()
+    
+    def create_gcp_bucket(self):
+        try:
+            bucket = self.storage_client.get_bucket(self.bucket_name)
+            logging.info(f"Bucket {bucket} present")
+        except Exception as e:
+            bucket = self.storage_client.create_bucket(self.bucket_name,location='us')
+            logging.info(f"Bucket {bucket} created")
+    
+    def gcs_check_file_exists(self,folder_position,folder_suffix,previous_folder='trade'):
+        self.object_exists = False
+        blobs = self.storage_client.list_blobs(self.bucket_name)
+        for blob in blobs:
+            parts = blob.name.split('/')
+            if ((parts[folder_position-2] == previous_folder) and (parts[folder_position-1].endswith(folder_suffix))):
+                logging.info(f"Object {blob.name} successfully created")
+                break
---
+++ Trade_Analysis_Capstone_Project/spark_airflow/dags/trade_analysis_pipeline.py
@@ -0,0 +1,95 @@
+"""
+This module is an end to end airflow data pipeline that ingests and processes daily stock
+market data from multiple stock exchanges. Data is read from and written to Google Cloud Storage.
+Once data is ingested, end of day corrections are processed and analytical data required 
+for business analysis is obtained.
+"""
+
+from airflow import DAG
+#from airflow.operators.python_operator import PythonOperator
+#from airflow.operators.bash_operator import BashOperator
+from airflow import models
+from airflow.providers.google.cloud.operators import dataproc
+from airflow.utils import trigger_rule
+import pendulum
+from datetime import date, datetime, timedelta
+
+local_tz = pendulum.timezone("America/Detroit")
+gcp_project_id = 'splendid-planet-367217'
+gcp_region = 'us-east1'
+gke_cluster = 'spark-airflow-cluster'
+dataproc_cluster = 'spark_airflow_dataproc'
+bucket_name = 'trade_quote_analysis'
+
+VIRTUAL_CLUSTER_CONFIG = {
+    "kubernetes_cluster_config": {
+        "gke_cluster_config": {
+            "gke_cluster_target": f"projects/{gcp_project_id}/locations/{gcp_region}/clusters/{gke_cluster}",
+            "node_pool_target": [
+                {
+                    "node_pool": f"projects/{gcp_project_id}/locations/{gcp_region}/clusters/{gke_cluster}/nodePools/dp",  # noqa
+                    "roles": ["DEFAULT"],
+                }
+            ],
+        },
+        "kubernetes_software_config": {"component_version": {"SPARK": b'3'}},
+    },
+    "staging_bucket": "trade-analysis-staging-bucket",
+}
+
+ingest_data_job = {
+    "reference": {"project_id": gcp_project_id},
+    "placement": {"cluster_name": dataproc_cluster},
+    "pyspark_job": {"main_python_file_uri": f"gs://{bucket_name}/ingest_trade_data.py",
+                    "python_file_uris":f"gs://{bucket_name}/spark_airflow/cloud_setup.py"}
+}
+
+trade_analysis_job = {
+    "reference": {"project_id": gcp_project_id},
+    "placement": {"cluster_name": dataproc_cluster},
+    "pyspark_job": {"main_python_file_uri": f"gs://{bucket_name}/spark_airflow/trade_analysis.py",
+                    "python_file_uris":f"gs://{bucket_name}/spark_airflow/cloud_setup.py"}
+}
+
+default_args = {
+            "owner": "airflow",
+            "start_date": datetime(2022, 11, 2,tzinfo=local_tz),
+            "depends_on_past": False,
+            "email_on_failure": False,
+            "retries": 2, # retry twice
+            "retry_delay": timedelta(minutes=5), # five minutes interval
+            "project_id":"splendid-planet-367217",
+            "region": "us-east1"
+        }
+
+dag = DAG(dag_id="TradeAnalysis",
+         schedule_interval="0 18 * * 1-5", # running at 6pm for weekdays
+         default_args=default_args,
+         description='Ingest and Analyze Stock Market Data' )
+
+create_cluster_in_gke = DataprocCreateClusterOperator(
+    task_id="create_cluster_in_gke",
+    cluster_name=dataproc_cluster,
+    project_id=gcp_project_id,
+    region=gcp_region,
+    virtual_cluster_config=VIRTUAL_CLUSTER_CONFIG
+)
+
+ingest_data = DataprocSubmitJobOperator(
+    task_id="ingest_data", job=ingest_data_job, region=gcp_region, project_id=gcp_project_id
+)
+
+trade_analysis = DataprocSubmitJobOperator(
+    task_id="trade_analysis", job=trade_analysis_job, region=gcp_region, project_id=gcp_project_id
+)
+
+delete_cluster = DataprocDeleteClusterOperator(
+    task_id="delete_cluster",
+    project_id=gcp_project_id,
+    cluster_name=dataproc_cluster,
+    region=gcp_region
+)
+
+
+create_cluster_in_gke >> ingest_data >> trade_analysis >> delete_cluster
+
